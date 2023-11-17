290edeb2d97f274c1867efba2e6a50782d22dbdf
alinelopesm <alinealopesm@gmail.com>
2023-11-17 16:36:24 -0300

    refactor: teste ambiente

--- database/index.js
+++ database/index.js
@@ -1,10 +1,20 @@
+/* Conexão remota rodar banco em nuvem */
 const connection = {
-    host: 'localhost',
-    port: 3306,
+    host: 'node159858-receitasbd.jelastic.saveincloud.net',
+    port: 12756,
     user: 'root',
-    password: '123456789',
+    password: 'LTVfqt11129',
     database: 'bd_receitas_gourmet'
 }
+
+/* Conexão local(rodar banco na maquina) */
+// const connection = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '123456789',
+//     database: 'bd_receitas_gourmet'
+// }
 
 const knex = require('knex')({
     client: 'mysql2',
